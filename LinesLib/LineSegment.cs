484a13fe63f413c5b04c5441cff7353249c30ddf
Tan <Tan@ACER-TK>
2024-09-04 10:07:20 +0200

    Add project files.

---
+++ LinesLib/LineSegment.cs
@@ -0,0 +1,38 @@
+﻿namespace LinesLib
+{
+    public class LineSegment
+    {
+        public int Start { get; }
+        public int End { get; }
+
+
+
+        public LineSegment(int start, int end)
+        {
+            Start = start;
+            End = end;
+
+            if (start > end)
+            {
+                throw new ArgumentOutOfRangeException($"Start should be before end");
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{Start}, {End}";
+        }
+
+        public bool Contains(int p)
+        {
+
+            if (Start <= p && p <= End)
+            {
+                return true;
+            }
+            return false;
+
+        }
+
+    }
+}
---
+++ LinesLibTests/LineSegmentTests.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LinesLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinesLib.Tests
+{
+    [TestClass()]
+    public class LineSegmentTests
+    {
+        [TestMethod()]
+        public void ConstructorTest()
+        {
+            // Arrange start is smaller than end
+            LineSegment smallerThanEnd = new LineSegment(1, 2);
+
+            // Assert
+            Assert.AreEqual(1, smallerThanEnd.Start);
+            Assert.AreEqual(2, smallerThanEnd.End);
+
+            // Arrange Act & Assert start is bigger than end
+            Assert.ThrowsException<ArgumentOutOfRangeException>(
+                () => new LineSegment(2, 1));
+
+            // Arrange start is equal to end
+            LineSegment samenumbers = new LineSegment(2, 2);
+
+            // Assert   
+            Assert.AreEqual(2, samenumbers.Start);
+            Assert.AreEqual(2, samenumbers.End);
+
+
+        }
+
+        [TestMethod()]
+
+        public void ToStringTest()
+        {
+            // Arrange 
+
+            LineSegment tostring = new LineSegment(1, 2);
+
+            // Act & Assert 
+
+            Assert.AreEqual("1, 2", tostring.ToString());
+        }
+
+
+        [TestMethod()]
+
+        public void ContainsTest()
+        {
+            // Arrange
+            LineSegment lineSegment1 = new LineSegment(1, 5);
+
+            // Act
+            bool isContanins = lineSegment1.Contains(4);
+
+            // Assert
+             // Act & Assert
+            Assert.IsTrue(lineSegment1.Contains(1));
+            Assert.IsTrue(lineSegment1.Contains(5));
+            // Arrange
+            LineSegment lineSegment2 = new LineSegment(1, 5);
+
+            // Act
+            bool isNotContains = lineSegment2.Contains(6);
+
+            // Assert
+            Assert.IsFalse(isNotContains);
+
+            // Act & Assert
+            Assert.IsFalse(lineSegment2.Contains(0));
+        }
+
+
+        [TestMethod()]
+
+        public void Contains2Test()
+        {
+            LineSegment lineControl = new LineSegment(1,10);
+        }
+    }
+}
